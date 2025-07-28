943e49cdbbb424061361b68790370f377a1ca99b
Pranav8459 <pranavgunge2006@gmail.com>
2025-07-28 18:04:35 +0530

    Practice

---
+++ python/demo repo.py
@@ -0,0 +1,11 @@
+a = input("Enter your name")
+if a == "piyush":
+    print("Kasa ahes bhau")
+elif a == "hasnain":
+    print("sup")
+elif a == "sarthak":
+    print("hello")
+elif a == "raj":
+    print("how's it going bro")
+else :
+    print("fuck off")
